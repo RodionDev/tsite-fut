522eb051b7daf6cd707f8d00d94156948b2fc24a
Rodion <rodion.developer1@gmail.com>
2022-03-21 10:56:30 +0000

    API Register and Login controllers are working fine

---
+++ database/migrations/2018_05_06_211752_add_relation_roles_to_users_table.php
@@ -0,0 +1,18 @@
+<?php
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+class AddRelationRolesToUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+        });
+    }
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreign('role_id')  ->references('id')->on('roles');
+        });
+    }
+}
