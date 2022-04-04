bef401e47d1e4235a9cfa4378b88943ba6fa0dbc
Rodion <rodion.developer1@gmail.com>
2022-04-04 23:25:01 +0000

    added meta tag for csrf token

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta nama="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('site-title') - {{ env('APP_NAME', 'FCH') }}</title>
     <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
     @hasSection('css')
