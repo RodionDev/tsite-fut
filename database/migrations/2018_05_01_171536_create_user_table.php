8130a1369cc1c6496f50c6810a387539f68c837d
Rodion <rodion.developer1@gmail.com>
2022-08-15 21:13:53 +0000

    fixed users being removed when role is deleted

--- database/migrations/2018_05_01_171536_create_user_table.php
+++ database/migrations/2018_05_01_171536_create_user_table.php
@@ -10,8 +10,7 @@ class CreateUserTable extends Migration
         {
             $table->increments('id');
             $table->integer('role_id')          ->unsigned();
-            $table->foreign('role_id')  ->references('id')->on('role')
-            ->onDelete('cascade');
+            $table->foreign('role_id')  ->references('id')->on('role');
             $table->string('email')             ->unique();
             $table->string('first_name')        ->nullable();
             $table->string('sur_name')          ->nullable();
