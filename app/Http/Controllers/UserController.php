82d31c32175ca5bd258fc129f518a79ec349b0a6
Rodion <rodion.developer1@gmail.com>
2022-08-31 19:23:41 +0000

    renamed $id to $role_id for clarification

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,14 +6,14 @@ use Illuminate\Support\Facades\Auth;
 use \Illuminate\Http\Response;
 class UserController extends Controller
 {
-    public function search($name=null, $id=null)
+    public function search($name=null, $role_id=null)
     {
         $name = ($name) ? $name : request()->name;
-        if(request()->id || $id)
+        if(request()->id || $role_id)
         {
-            $id = ($id) ? $id : request()->id;
+            $role_id = ($role_id) ? $role_id : request()->id;
             return response()->json(
-                User::searchName($name, $id)    ->distinct()->get()
+                User::searchName($name, $role_id)    ->distinct()->get()
             );
         }
         else
