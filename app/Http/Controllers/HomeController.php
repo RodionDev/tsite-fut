cd3a0e6a2fc7647f0f9f68061bf2815970d3531c
Rodion <rodion.developer1@gmail.com>
2022-10-07 11:19:51 +0000

    Redirect to correct tournament when visiting home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,7 +6,12 @@ class HomeController extends Controller
 {
     public function index()
     {
-        if(Auth::check())   return redirect(route('tournament'));
+        if(Auth::check())
+        {
+            $user = Auth::user();
+            $tournament = $user->tournaments(true)->first();
+            return redirect(route('tournament', $tournament->id));
+        }
         else                return redirect(route('login'));
     }
 }
