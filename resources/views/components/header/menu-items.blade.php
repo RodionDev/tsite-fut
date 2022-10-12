a6a75b076b972902260fe531a0586e9469b9ab12
Rodion <rodion.developer1@gmail.com>
2022-10-12 01:51:31 +0000

    Removed current menu item and made logo clickable

--- resources/views/components/header/menu-items.blade.php
+++ resources/views/components/header/menu-items.blade.php
@@ -1,6 +1,5 @@
 <div class="nav-content">
     <ul class="tabs tabs-transparent">
-        <li class="tab"><a target="_self" href="/toernooi">Huidig</a></li>
         <li class="tab"><a target="_self" href="/toernooien">Toernooien</a></li>
         <li class="tab"><a target="_self" href="/teams">Teams</a></li>
         <li class="tab"><a target="_self" href="/manofthematch">MOTM</a></li>
