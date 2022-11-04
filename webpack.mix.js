ad1f515d357bc5312a35521a28ffe409de9adcfe
Rodion <rodion.developer1@gmail.com>
2022-11-04 23:17:25 +0000

    js compilation bug

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,6 @@
 let mix = require('laravel-mix');
 mix
-    .scripts(
+    .babel(
         [
             'node_modules/materialize-css/dist/js/materialize.min.js',
             'resources/assets/js/app.js',
@@ -9,17 +9,17 @@ mix
         'public/js/app.js'
     )
    .sass('resources/assets/sass/app.scss', 'public/css')
-   .scripts(
+   .babel(
        ['resources/assets/js/components/pool-classification.js'],
        'public/js/pages/tournament.js'
    )
    .sass('resources/assets/sass/pages/tournament.scss', 'public/css/pages')
-   .scripts(
+   .babel(
        ['resources/assets/js/pages/cud-tournament.js'],
        'public/js/pages/cud-tournament.js'
     )
    .sass('resources/assets/sass/pages/teams.scss', 'public/css/pages')
-   .scripts(
+   .babel(
        ['resources/assets/js/pages/new-team-form.js'],
        'public/js/pages/new-team.js'
     );
