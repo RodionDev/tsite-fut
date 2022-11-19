6d4ead300315032f71af92ba7cc774ad6808ba6a
Rodion <rodion.developer1@gmail.com>
2022-11-19 17:34:35 +0000

    Show score of finished matches

--- resources/views/components/matches-list.blade.php
+++ resources/views/components/matches-list.blade.php
@@ -138,13 +138,14 @@
                             <div class="col s1">
                                 <i class="mdi mdi-36px mdi-menu-down center"></i>
                             </div>
-                            <div class="col s12 center">
-                                @if($match->start)
-                                {{ date_format(date_create($match->start), "H:i") }}
-                                @else
-                                Onbekend
-                                @endif
+                            <div class="col s1"></div>
+                            <div class="col s5 center">
+                                {{ $match->result1->score }}
                             </div>
+                            <div class="col s5 center">
+                                {{ $match->result2->score }}
+                            </div>
+                            <div class="col s1"></div>
                         </div>
                     </div>
                     <div class="collapsible-body center">
@@ -154,6 +155,11 @@
                         @else
                         <span class="font-size-5">Onbekend</span></br>
                         @endif
+                        <div class="row">
+                            <div class="col s12 m6">
+                                <img class="team-logo vertical-centre-image" src="{{ $match->result2->team->logo or asset('images\image-missing.png') }}">
+                            </div>
+                        </div>
                         @if($permission == 20 || $permission >= 50)
                         <a class="waves-effect waves-light btn right" href="{{ route('match.edit.route', $match->id) }}">Wijzigen</a></br>
                         @endif
