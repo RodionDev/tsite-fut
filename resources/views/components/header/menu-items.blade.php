336fe5714f7f6147913ea033009291f2bc17eef4
Rodion <rodion.developer1@gmail.com>
2023-02-17 20:48:34 +0000

    fixed error on login page relating to menu-items

--- resources/views/components/header/menu-items.blade.php
+++ resources/views/components/header/menu-items.blade.php
@@ -3,7 +3,7 @@
         <li class="tab"><a target="_self" href="/toernooien">Toernooien</a></li>
         <li class="tab"><a target="_self" href="/teams">Teams</a></li>
         <li class="tab"><a target="_self" href="/profiel">Profiel</a></li>
-        @if(Auth::user()->role()->first()->permission >= 30)
+        @if(Auth::check() && Auth::user()->role()->first()->permission >= 30)
         <li class="tab"><a target="_self" href="/uitnodigen">Beheer</a></li>
         @endif
     </ul>
