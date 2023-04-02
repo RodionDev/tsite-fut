b25c9565ad2794ec074a5fe1594412337477c2af
Rodion <rodion.developer1@gmail.com>
2023-04-02 10:50:03 +0000

    Terms of service view

--- resources/views/pages/auth/update-user.blade.php
+++ resources/views/pages/auth/update-user.blade.php
@@ -65,8 +65,10 @@
                         <input id="token" type="text" name="token" value="{{ $register_token }}" required />
                     </div>
                     <div class="col s12 input-field">
-                        <input type="checkbox" required>
-                    <label>Ja, ik accepteer de <a href="{{ route('terms.of.service') }}" target="_blank">Algemene voorwaarden</a>  van mijntoernooien.nl. (link opent in een nieuw venster)</label>
+                        <label>
+                            <input name="terms-of-service" type="checkbox" required>
+                            <span>Ja, ik accepteer de <a href="{{ route('terms.of.service') }}" target="_blank">Algemene voorwaarden</a>  van mijntoernooien.nl. (link opent in een nieuw venster)</span>
+                        </label>
                     </div>
                     @endif
                     <div class="col s12 input-field">
