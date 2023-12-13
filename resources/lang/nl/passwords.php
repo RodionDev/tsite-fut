20aeec979986c1893a4c3cb990e3a26930e6e458
Rodion <rodion.developer1@gmail.com>
2023-12-13 10:23:40 +0000

    translation error dutch

--- resources/lang/nl/passwords.php
+++ resources/lang/nl/passwords.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'password' => 'Wachtwoord moet minimaal zes tekens lang zijn en de wachtwoorden moeten overeenkomen.',
+    'password' => 'Wachtwoord moet minimaal vijf tekens lang zijn en de wachtwoorden moeten overeenkomen.',
     'reset' => 'Het wachtwoord van uw account is gewijzigd.',
     'sent' => 'We hebben een e-mail verstuurd met instructies om een nieuw wachtwoord in te stellen.',
     'token' => 'Dit wachtwoordhersteltoken is niet geldig.',
