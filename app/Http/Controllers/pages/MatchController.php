cbe9ff18d26ae86b7370c9318f1607383eb54f9e
Rodion <rodion.developer1@gmail.com>
2024-05-07 13:05:50 +0000

    fixining the fix for sorting

--- app/Http/Controllers/pages/MatchController.php
+++ app/Http/Controllers/pages/MatchController.php
@@ -251,15 +251,6 @@ class MatchController extends Controller
         }
         $teams = $this->checkArrayDuplicateTeam($teams_has_played);
         $teams = $this->mergeTeams($teams, $teams_has_not_played);
-        $teams_sort_points;
-        $teams_sort_goals_saldo;
-        foreach ($teams as $key => $team) 
-        {
-            $team->saldo = $team->goals - $team->countergoals;
-            $teams[$key] = $team;
-            $teams_sort_points[$key] = $team->points;
-            $teams_sort_goals_saldo[$key] = $team->saldo;
-        }
         foreach ($teams_has_not_played as $x => $_team) 
         {
             foreach ($teams as $y => $team) 
@@ -271,6 +262,15 @@ class MatchController extends Controller
             }
         }
         $teams = $teams_has_not_played;
+        $teams_sort_points;
+        $teams_sort_goals_saldo;
+        foreach ($teams as $key => $team) 
+        {
+            $team->saldo = $team->goals - $team->countergoals;
+            $teams[$key] = $team;
+            $teams_sort_points[$key] = $team->points;
+            $teams_sort_goals_saldo[$key] = $team->saldo;
+        }
         array_multisort($teams_sort_points, SORT_DESC, $teams_sort_goals_saldo, SORT_DESC, $teams);
         return view('pages.scoreboard', compact('teams'));
     }
